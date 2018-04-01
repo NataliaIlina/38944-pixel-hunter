b13c7b78e4fb8f48d1a9335240b91c8031bec685
Natalia_Ilina <metlinova86@gmail.com>
2018-04-01 13:22:59 +0300

    Обвновлен файл main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
-const keyCodes = {
-  arrowRight: 39,
-  arrowLeft: 37
-};
-const mainScreen = document.querySelector(`.central`);
-const screens = [`#greeting`, `#rules`, `#game-1`, `#game-2`, `#game-3`, `#stats`].map((id) => document.querySelector(id));
+import nextScreen from './templates/intro.js';
+import showScreen from './utils/show-screen.js';
 
-let currentScreenNumber = 0;
-
-// копируем шаблон и вставляем его вместо главного экрана
-const showScreen = (index) => {
-  let template = screens[index].content.cloneNode(true);
-  mainScreen.innerHTML = ``;
-  mainScreen.appendChild(template);
-};
-
-// показываем приветственный экран
-showScreen(currentScreenNumber);
-
-// отслеживаем нажатие стрелок + Alt и меняем экраны
-document.addEventListener(`keydown`, function (evt) {
-  if (evt.altKey) {
-    if (evt.keyCode === keyCodes.arrowRight && currentScreenNumber < screens.length - 1) {
-      currentScreenNumber++;
-    }
-    if (evt.keyCode === keyCodes.arrowLeft && currentScreenNumber > 0) {
-      currentScreenNumber--;
-    }
-  }
-  showScreen(currentScreenNumber);
-});
+showScreen(nextScreen);
