da9971b17214bae4cefa8a3d534531081671cf4a
Natalia_Ilina <metlinova86@gmail.com>
2018-04-12 16:54:16 +0300

    Добавлены объекты игры

---
+++ js/data/data.js
@@ -0,0 +1,39 @@
+const LevelType = {
+  FIRST: {
+    task: `Угадайте для каждого изображения фото или рисунок?`,
+    images: {
+      paint: `https://k42.kn3.net/CF42609C8.jpg`,
+      photo: `http://i.imgur.com/1KegWPz.jpg`
+    },
+    answers: {
+      question1: `paint`,
+      question2: `photo`
+    }
+  },
+  SECOND: {
+    task: `Угадай, фото или рисунок?`,
+    images: {
+      photo: `https://i.imgur.com/DiHM5Zb.jpg`
+    },
+    answers: {
+      question1: `photo`,
+    }
+  },
+  THIRD: {
+    task: `Найдите рисунок среди изображений`,
+    images: {
+      photo: `https://i.imgur.com/DiHM5Zb.jpg`,
+      paint: `https://k42.kn3.net/D2F0370D6.jpg`
+    },
+    answers: {
+      question1: `photo`,
+    }
+  }
+};
+
+const game = {
+  time: 30,
+  lives: 2,
+};
+
+export {game, LevelType};
