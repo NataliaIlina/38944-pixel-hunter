2d7204dda05f386a8d0d464640f8077f26b07584
Natalia_Ilina <metlinova86@gmail.com>
2018-04-13 11:21:53 +0300

    Исправила шаблон шапки

--- js/templates/header.js
+++ js/templates/header.js
@@ -1,17 +1,25 @@
-import renderButton from './back-button.js';
+import greetingScreen from '../templates/greeting.js';
+import showScreen from '../utils/show-screen.js';
+import getElementFromTemplate from '../utils/create-elem.js';
 
 const renderHeader = (game) => {
-  const header = document.createElement(`header`);
-  header.classList.add(`header`);
-  if (game) {
-    header.innerHTML = `
-    <h1 class="game__timer">${game.time}</h1>
+  const headerTemplate = `<header class="header">
+    <div class="header__back">
+      <button class="back">
+        <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+        <img src="img/logo_small.svg" width="101" height="44">
+      </button>
+    </div>
+    ${game ? `<h1 class="game__timer">${game.time}</h1>
     <div class="game__lives">
       ${new Array(3 - game.lives).fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`).join(``)}
       ${new Array(game.lives).fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`).join(``)}
-    </div>`;
-  }
-  header.insertAdjacentElement(`afterbegin`, renderButton());
+    </div>` : ``}
+    </header>`;
+  const header = getElementFromTemplate(headerTemplate);
+  header.querySelector(`.back`).addEventListener(`click`, () => {
+    showScreen(greetingScreen);
+  });
   return header;
 };
 
