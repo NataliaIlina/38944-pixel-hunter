78eb476db88381fafb40eb0c78d843b173d938f2
Natalia_Ilina <metlinova86@gmail.com>
2018-04-15 17:20:01 +0300

    Доработан модуль обработки ответов

--- js/templates/results.js
+++ js/templates/results.js
@@ -1,56 +1,34 @@
 import getElementFromTemplate from '../utils/create-elem.js';
 
-const answerType = {
-  fast: (answer) => {
-    return answer.isCorrect && answer.time < 10;
-  },
-  slow: (answer) => {
-    return answer.isCorrect && answer.time > 20;
-  },
-  correct: (answer) => {
-    return answer.isCorrect && answer.time <= 20 && answer.time >= 10;
-  },
-  wrong: (answer) => {
-    return !answer.isCorrect;
+const answerType = (answer) => {
+  let className;
+  if (answer.isCorrect) {
+    className = `correct`;
   }
+  if (answer.isCorrect && answer.time < 10) {
+    className = `fast`;
+  }
+  if (answer.isCorrect && answer.time > 20) {
+    className = `slow`;
+  }
+  if (!answer.isCorrect) {
+    className = `wrong`;
+  }
+  return className;
 };
 
 const renderResult = (answers) => {
+  const classNames = answers.map((answer) => answerType(answer));
+
   const template = `
   <div class="stats">
     <ul class="stats">
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
+      ${classNames.map((className) => `<li class="stats__result stats__result--${className}"></li>`)}
+      ${new Array(10 - classNames.length).fill(`<li class="stats__result stats__result--unknown"></li>`).join(``)}
     </ul>
   </div>`;
 
   const result = getElementFromTemplate(template);
-  const resultElems = Array.from(result.querySelectorAll(`li`));
-
-  answers.forEach((answer, index) => {
-    resultElems[index].classList.remove(`stats__result--unknown`);
-    if (answerType.fast(answer)) {
-      resultElems[index].classList.add(`stats__result--fast`);
-    }
-    if (answerType.slow(answer)) {
-      resultElems[index].classList.add(`stats__result--slow`);
-    }
-    if (answerType.correct(answer)) {
-      resultElems[index].classList.add(`stats__result--correct`);
-    }
-    if (answerType.wrong(answer)) {
-      resultElems[index].classList.add(`stats__result--wrong`);
-    }
-  });
-
   return result;
 };
 
