09637552ff2912b6fd6b0923378cfd31ddaaa966
Natalia_Ilina <metlinova86@gmail.com>
2018-04-20 15:50:59 +0300

    Стили модального окна перемещены в другой файл

--- js/templates/game/modal-view.js
+++ js/templates/game/modal-view.js
@@ -6,20 +6,20 @@ class ModalView extends AbstractView {
   }
 
   get template() {
-    return `<div class="modal">
-        <p class="modal__title">
+    return `<div class="game__modal">
+        <p class="game__modal-title">
           Все результаты текущей игры будут утеряны.<br /> Хотите выйти?
         </p>
-        <button type="button" class="modal__button  modal__button--reset">Да</button>
-        <button type="button" class="modal__button  modal__button--continue">Нет</button>
+        <button type="button" class="game__modal-button  game__modal-button--reset">Да</button>
+        <button type="button" class="game__modal-button  game__modal-button--continue">Нет</button>
       </div>`;
   }
 
   bind() {
-    this.element.querySelector(`.modal__button--reset`).addEventListener(`click`, () => {
+    this.element.querySelector(`.game__modal-button--reset`).addEventListener(`click`, () => {
       this.onResetClick();
     });
-    this.element.querySelector(`.modal__button--continue`).addEventListener(`click`, () => {
+    this.element.querySelector(`.game__modal-button--continue`).addEventListener(`click`, () => {
       this.onContinueClick();
     });
   }
